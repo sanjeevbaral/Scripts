975b3e5d172f873beb36b4a29a03f07ba5ebac15
Sanjeev Baral <baral.sanjeev@gmail.com>
2018-11-10 10:03:57 -0500

    test

---
+++ Decipher/cipher/CipherTest/frmEncrypter.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace CipherTest
+{
+    partial class frmEncrypter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtConnectionString = new System.Windows.Forms.TextBox();
+            this.btnTest = new System.Windows.Forms.Button();
+            this.btnEncryptDecrypt = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCipherPhrase = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Connection string";
+            // 
+            // txtConnectionString
+            // 
+            this.txtConnectionString.Location = new System.Drawing.Point(15, 37);
+            this.txtConnectionString.Multiline = true;
+            this.txtConnectionString.Name = "txtConnectionString";
+            this.txtConnectionString.Size = new System.Drawing.Size(557, 47);
+            this.txtConnectionString.TabIndex = 1;
+            // 
+            // btnTest
+            // 
+            this.btnTest.Location = new System.Drawing.Point(463, 90);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(109, 23);
+            this.btnTest.TabIndex = 2;
+            this.btnTest.Text = "Test Connection";
+            this.btnTest.UseVisualStyleBackColor = true;
+            this.btnTest.Click += new System.EventHandler(this.btnTest_Click);
+            // 
+            // btnEncryptDecrypt
+            // 
+            this.btnEncryptDecrypt.Location = new System.Drawing.Point(497, 169);
+            this.btnEncryptDecrypt.Name = "btnEncryptDecrypt";
+            this.btnEncryptDecrypt.Size = new System.Drawing.Size(75, 23);
+            this.btnEncryptDecrypt.TabIndex = 3;
+            this.btnEncryptDecrypt.Text = "Encrypt";
+            this.btnEncryptDecrypt.UseVisualStyleBackColor = true;
+            this.btnEncryptDecrypt.Click += new System.EventHandler(this.btnEncryptDecrypt_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 127);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Cipher Phrase";
+            // 
+            // txtCipherPhrase
+            // 
+            this.txtCipherPhrase.Location = new System.Drawing.Point(20, 143);
+            this.txtCipherPhrase.Name = "txtCipherPhrase";
+            this.txtCipherPhrase.Size = new System.Drawing.Size(552, 20);
+            this.txtCipherPhrase.TabIndex = 5;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(497, 206);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 6;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // frmEncrypter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 241);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtCipherPhrase);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnEncryptDecrypt);
+            this.Controls.Add(this.btnTest);
+            this.Controls.Add(this.txtConnectionString);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmEncrypter";
+            this.Text = "Connection String Encryptor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtConnectionString;
+        private System.Windows.Forms.Button btnTest;
+        private System.Windows.Forms.Button btnEncryptDecrypt;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtCipherPhrase;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
+
---
+++ Decipher/cipher/CipherTest/frmEncrypter.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace CipherTest
+{
+    public partial class frmEncrypter : Form
+    {
+        bool Encrypted = false;
+        SqlConnection con;
+        public frmEncrypter()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                con = new SqlConnection();
+                con.ConnectionString = txtConnectionString.Text;
+                con.Open();
+                con.Close();
+                MessageBox.Show("Connection successful");
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnEncryptDecrypt_Click(object sender, EventArgs e)
+        {
+            if(!Encrypted)
+            {
+                string cipherText = CipherSuite.Cipher.Encrypt(txtConnectionString.Text, txtCipherPhrase.Text);
+                txtConnectionString.Text = cipherText;
+                btnEncryptDecrypt.Text = "Decrypt";
+                Encrypted = true;
+            }
+            else
+            {
+                string clearText = CipherSuite.Cipher.Decrypt(txtConnectionString.Text, txtCipherPhrase.Text);
+                txtConnectionString.Text = clearText;
+                btnEncryptDecrypt.Text = "Encrypt";
+                Encrypted = false;
+
+            }
+
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog dlg = new SaveFileDialog();
+            dlg.FileName = "connection.inf";
+            dlg.ShowDialog();
+            if(dlg.FileName!="")
+            {
+                StreamWriter writer = new StreamWriter(dlg.FileName);
+                writer.WriteLine(txtConnectionString.Text);
+                writer.Flush();
+                writer.Close();
+            }
+           
+
+
+        }
+    }
+}
